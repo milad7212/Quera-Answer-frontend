f22e3e08e71f85ce883e14c51ea1d4d0c59a7cca
miillaadddddd <milad7212@gmail.com>
2022-03-06 21:27:34 +0330

    solve : poll-creator

--- React/poll-creator/src/components/DescriptionItem.js
+++ React/poll-creator/src/components/DescriptionItem.js
@@ -1,5 +1,36 @@
 import { ShowEmoji } from "./ShowEmoji";
 
 export const DescriptionItem = ({ emojis, setEmojis }) => {
-  return null
+  const handelChangeDescription = (e, index) => {
+    let selectedemoji = e.target.value;
+
+    let newEmojis = [...emojis];
+
+    newEmojis[index].description = selectedemoji;
+    setEmojis(newEmojis);
+  };
+  return (
+    <>
+      {emojis.map((emoji, index) => (
+        <>
+          {emoji.isSelected && (
+            <div key={index}>
+              <label htmlFor={emoji.title}>
+                <ShowEmoji
+                  src={emoji.src}
+                  viewStyle="description"
+                  name={emoji.title}
+                />
+              </label>
+              <input
+                type="text"
+                id={emoji.title}
+                onChange={(e) => handelChangeDescription(e, index)}
+              />
+            </div>
+          )}
+        </>
+      ))}
+    </>
+  );
 };
--- React/poll-creator/src/components/ShowPreviewEmoji.js
+++ React/poll-creator/src/components/ShowPreviewEmoji.js
@@ -1,5 +1,20 @@
 import { ShowEmoji } from "./ShowEmoji";
 
 export const ShowPreviewEmoji = ({ emojis }) => {
-  return null
+  console.log('emojis', emojis)
+  
+  return (
+    <>
+      {emojis.map((emoji, index) => (
+        <>
+        {emoji.isSelected && 
+        <>
+          <ShowEmoji key={index} src={emoji.src} viewStyle='preview' name={emoji.title} />
+          <div >{emoji.description}</div>
+          </>
+        }
+        </>
+      ))}
+    </>
+  );
 };
