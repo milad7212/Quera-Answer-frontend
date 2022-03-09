24e1dd2415a7a2915403d4ec293a240f79a5df1e
miillaadddddd <milad7212@gmail.com>
2022-03-09 17:41:18 +0330

    try

--- React/orderbook/src/App.jsx
+++ React/orderbook/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import OrderbookHeader from "./components/OrderbookHeader";
+import OrderbookListItem from "./components/OrderbookListItem";
+import { sortBuyData, sortSellData } from "./functions";
+import { ORDER_TYPE } from "./types";
 
 function App({ data }) {
+  let sellData = sortSellData(data);
+  let buyData = sortBuyData(data);
+
   return (
     <div className="container">
       <div className="wrapper">
@@ -10,11 +16,33 @@ function App({ data }) {
           <div className="sell-side side">
             {
               // Loop through sell orders and use OrderbookListItem component
+              sellData.map((item, index) => (
+                <>
+                  <OrderbookListItem
+                    index={index}
+                    type={ORDER_TYPE.SELL}
+                    price={item.price}
+                    amount={item.quantity}
+                    bgWidth={10}
+                  />
+                </>
+              ))
             }
           </div>
           <div className="buy-side side">
             {
               // Loop through buy orders and use OrderbookListItem component
+              buyData.map((item, index) => (
+                <>
+                  <OrderbookListItem
+                    index={index}
+                    type={ORDER_TYPE.SELL}
+                    price={item.price}
+                    amount={item.quantity}
+                    bgWidth={10}
+                  />
+                </>
+              ))
             }
           </div>
         </div>
--- React/orderbook/src/functions/index.js
+++ React/orderbook/src/functions/index.js
@@ -1,1 +1,12 @@
 // You can use this file to define your functions
+import { ORDER_TYPE } from "../types";
+export const sortSellData = (data) => {
+    return data
+        .filter((item) => item.type == ORDER_TYPE.SELL)
+        .sort((a, b) => a.price - b.price);
+};
+export const sortBuyData = (data) => {
+    return data
+        .filter((item) => item.type == ORDER_TYPE.BUY)
+        .sort((a, b) => b.price - a.price);
+};
