a0e8616cc473a00736788ac02c24169271e6c547
miillaadddddd <milad7212@gmail.com>
2022-03-13 18:13:46 +0330

    try to solve

--- React/orderbook/src/functions/index.js
+++ React/orderbook/src/functions/index.js
@@ -1,12 +1,40 @@
 // You can use this file to define your functions
 import { ORDER_TYPE } from "../types";
 export const sortSellData = (data) => {
-    return data
+    let obj = {};
+    let arr = [];
+    data
         .filter((item) => item.type == ORDER_TYPE.SELL)
-        .sort((a, b) => a.price - b.price);
+        .sort((a, b) => a.price - b.price)
+        .map((el) => {
+            if (obj[el.price]) {
+                obj[el.price] = obj[el.price] + el.quantity;
+            } else {
+                obj[el.price] = el.quantity;
+            }
+        });
+    for (let [key, value] of Object.entries(obj)) {
+        arr.push({ quantity: value, price: key, type: "sell" });
+        console.log(key, value);
+    }
+    return arr;
 };
 export const sortBuyData = (data) => {
-    return data
+    let obj = {};
+    let arr = [];
+    data
         .filter((item) => item.type == ORDER_TYPE.BUY)
-        .sort((a, b) => b.price - a.price);
+        .sort((a, b) => b.price - a.price)
+        .map((el) => {
+            if (obj[el.price]) {
+                obj[el.price] = obj[el.price] + el.quantity;
+            } else {
+                obj[el.price] = el.quantity;
+            }
+        });
+    for (let [key, value] of Object.entries(obj)) {
+        arr.push({ quantity: value, price: key, type: "buy" });
+        console.log(key, value);
+    }
+    return arr;
 };
