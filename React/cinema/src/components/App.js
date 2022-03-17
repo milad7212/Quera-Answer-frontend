e5945a8fb5550d8392e104602690369f15656c1c
miillaadddddd <milad7212@gmail.com>
2022-03-17 13:05:37 +0330

    try

---
+++ React/cinema/src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MovieDetail from "./MovieDetail";
+import NotFound from "./NotFound";
+export default function App() {
+  // your code here ...
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/about"></Route>
+          <Route path="/users"></Route>
+          <Route path="/">
+            <MovieDetail />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
