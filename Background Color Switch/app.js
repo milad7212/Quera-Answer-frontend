da944ced698379917ba49cd0d494ad1493cb4d5f
miillaadddddd <milad7212@gmail.com>
2022-03-19 14:18:00 +0330

    Solve : Background Color Switch

---
+++ Background Color Switch/app.js
@@ -0,0 +1,25 @@
+const btn1 = document.getElementById('grey');
+const btn2 = document.getElementById('white');
+const btn3 = document.getElementById('blue');
+const btn4 = document.getElementById('yellow');
+
+btn1.addEventListener('click', function onClick() {
+    document.body.style.backgroundColor = 'grey';
+ 
+  
+});
+btn2.addEventListener('click', function onClick() {
+    document.body.style.backgroundColor = 'white';
+ 
+  
+});
+btn3.addEventListener('click', function onClick() {
+    document.body.style.backgroundColor = 'blue';
+ 
+  
+});
+btn4.addEventListener('click', function onClick() {
+    document.body.style.backgroundColor = 'yellow';
+ 
+  
+});
