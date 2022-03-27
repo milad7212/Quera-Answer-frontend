a027c79a53cd9ce441bde49135dbfe5bd4863b82
miillaadddddd <milad7212@gmail.com>
2022-03-27 17:14:54 +0430

    TRY

--- React/theme-switch-reduxx/src/App.jsx
+++ React/theme-switch-reduxx/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useSelector } from "react-redux";
 import NameBox from "./components/NameBox";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import { THEME_TYPE } from "./constants";
@@ -7,9 +8,9 @@ import Styles from "./data/Styles";
 
 // No need to change *return* part in both StyleTag and App components
 // You have to set themeMode based on redux state
-
 const StyleTag = () => {
-  const themeMode = THEME_TYPE.LIGHT;
+ const theme = useSelector(state => state.theme)
+  const themeMode = THEME_TYPE[theme];
   return (
     <Helmet>
       <style>{Styles(themeMode)}</style>
