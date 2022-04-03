075ccad0dc28a6722473ecedf6643ef919f66db4
miillaadddddd <milad7212@gmail.com>
2022-04-03 10:27:18 +0430

    try

--- React/iran-map/src/components/IranMap.js
+++ React/iran-map/src/components/IranMap.js
@@ -1,56 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import './IranMap.css';
+import "./IranMap.css";
 
-import CityModal from './CityModal';
+import CityModal from "./CityModal";
 
+const IranMap = () => {
+  const [citiesData, setCitiesData] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-class IranMap extends React.Component {
-
-    state = {
-        citiesData: null,
-        selectedCity: null,
-        isModalOpen: false,
-    };
-
-
-    cityClicked = (id) => (event) => {
-        event.preventDefault();
-        // Fetch city details and open modal
-    };
-
-    closeModal = () => {
-        this.setState({
-            isModalOpen: false,
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <div className="map-container">
-                    {(this.state.citiesData || []).map((record) => (
-                        <div
-                            key={record.id}
-                            className="city-name"
-                            style={{
-                                top: `${record.top}%`,
-                                left: `${record.left}%`,
-                            }}
-                            onClick={this.cityClicked(record.id)}
-                        >
-                            {record.name}
-                        </div>
-                    ))}
-                </div>
-                <CityModal
-                    city={this.state.selectedCity}
-                    isOpen={this.state.isModalOpen}
-                    onClose={this.closeModal}
-                />
-            </div>
-        );
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(decodeURI(`http://localhost:9000/cities/`));
+      let dataFinal = await response.text();
+      let message = await JSON.parse(dataFinal);
+      setCitiesData(message);
+      console.log("ans :>> ", message);
+      // setCitiesData(message);
     }
-}
+    fetchData();
+  }, []);
+
+  const cityClicked = (id) => (event) => {
+    event.preventDefault();
+    // Fetch city details and open modal
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="map-container">
+        {(citiesData || []).map((record) => (
+          <div
+            key={record.id}
+            className="city-name"
+            style={{
+              top: `${record.top}%`,
+              left: `${record.left}%`,
+            }}
+            onClick={cityClicked(record.id)}
+          >
+            {record.name}
+          </div>
+        ))}
+      </div>
+      <CityModal
+        city={selectedCity}
+        isOpen={isModalOpen}
+        onClose={closeModal}
+      />
+    </div>
+  );
+};
 
 export default IranMap;
