b4cbf1b8d2fddacd5b5566b9a8f85ad3767ed477
miillaadddddd <milad7212@gmail.com>
2022-04-09 16:44:44 +0430

    solve  timer-hook

--- React/timer-hook/src/use-timer.js
+++ React/timer-hook/src/use-timer.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
 
 export function useTimer() {
-  // ...
-  return {}
+    const [seconds, setSeconds] = useState(0);
+    const [isStop, setIsStop] = useState(false);
+    useEffect(() => {
+        if (!isStop) {
+            const timing = setInterval(() => {
+                setSeconds(seconds + 1);
+            }, 1000);
+            return () => clearInterval(timing);
+        }
+    }, [seconds, isStop]);
+    const reset = () => {
+        setSeconds(0);
+        setIsStop(false);
+    };
+    const stop = () => {
+        setIsStop(true);
+    };
+    const resume = () => {
+        setIsStop(false);
+    };
+    // ...
+    return { seconds, reset, stop, resume };
 }
+
+// const [secounds, setSecounds] = useState(0);
+// useEffect(() => {
+//     const timing = setInterval(() => {
+//         setSecounds(secounds + 1);
+//     }, 1000);
+//     return () => clearInterval(timing);
+// }, []);
+
+// const stop = () => {};
+// const resume = () => {};
+// const reset = () => {};
+// // ...
+// return { secounds };
