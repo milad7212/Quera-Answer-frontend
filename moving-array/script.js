d49aa880fda8e2ec398c8cae21b05ff43608c509
miillaadddddd <milad7212@gmail.com>
2022-04-10 16:46:54 +0430

    Add moving-array

---
+++ moving-array/script.js
@@ -0,0 +1,12 @@
+const CHANGE_TYPE = {
+  UP: "UP",
+  DOWN: "DOWN",
+};
+const ERROR_TYPE = {
+  NOT_FOUND: "NOT_FOUND",
+  NOT_POSSIBLE: "NOT_POSSIBLE",
+  INVALID_INPUT: "INVALID_INPUT",
+};
+let numbers = [4, 6, 10, 23, 0, 24, 30, 2];
+
+// Your code here...
