7a79fec49ea8ebb40827f74d6f718776cf1d1245
miillaadddddd <milad7212@gmail.com>
2022-04-10 17:11:00 +0430

    try

--- moving-array/script.js
+++ moving-array/script.js
@@ -1,12 +1,22 @@
 const CHANGE_TYPE = {
-  UP: "UP",
-  DOWN: "DOWN",
+    UP: "UP",
+    DOWN: "DOWN",
 };
 const ERROR_TYPE = {
-  NOT_FOUND: "NOT_FOUND",
-  NOT_POSSIBLE: "NOT_POSSIBLE",
-  INVALID_INPUT: "INVALID_INPUT",
+    NOT_FOUND: "NOT_FOUND",
+    NOT_POSSIBLE: "NOT_POSSIBLE",
+    INVALID_INPUT: "INVALID_INPUT",
 };
 let numbers = [4, 6, 10, 23, 0, 24, 30, 2];
 
 // Your code here...
+
+document.addEventListener("DOMContentLoaded", function() {
+    let numContainer = document.getElementById('numbers-container')
+
+    numbers.map((num) => {
+        let newSpan = document.createElement('span');
+        numContainer.appendChild(newSpan);
+        newSpan.innerHTML = num;
+    })
+});
